39524597feb3ca08bbe18161ee43c9ce7071d6b9
Yinsong Xu <yinsongxu@gmail.com>
2014-10-15 23:24:42 -0700

    Start the 1001
    The origin of the problem:
    http://poj.org/problem?id=1001

---
+++ 1001/1001.c
@@ -0,0 +1,17 @@
+/*PKU Judge Online 1001	*/
+/*From: 	Yinsong Xu	*/
+/*Begin: 	Oct 15,2014	*/
+/*End:					*/
+
+#include <stdio.h>		//basic I/O head file. To use scanf() printf()
+#define NUM_INPUT 6		//Set the total input number to 6
+#define MAX_DIGIT 200	//Set the max digits of the array which store the input numbers
+
+/*Main function*/
+int main(){
+	char* base[NUM_INPUT];
+	int base[NUM_INPUT][MAX_DIGIT];
+	int result[NUM_INPUT][MAX_DIGIT];
+	int point[NUM_INPUT];
+	int digit[NUM_INPUT];
+}
