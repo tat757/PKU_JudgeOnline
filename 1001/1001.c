6a2aa4e19c26832eecfa691379011a246bebff62
Yinsong Xu <yinsongxu@gmail.com>
2014-10-27 11:33:06 -0700

    Changing to multiple inputs
    The way to input has changed from 6 inputs only to multiple inputs

--- 1001/1001.c
+++ 1001/1001.c
@@ -12,7 +12,7 @@
 #include <stdio.h>		//basic I/O head file. To use scanf() printf()
 #include <stdlib.h>		//To use exit()
 #include <string.h>		//To use strlen() memset()
-#define NUM_INPUT 6		//Set the total input number to 6
+//#define NUM_INPUT 6		//Set the total input number to 6
 #define MAX_DIGIT 200	//Set the max digits of the array which store the input numbers
 #define MAX_RESULT_DIGIT 200*2+10 		//Set the max digit of result
 
@@ -20,117 +20,110 @@
 /*Main function*/
 int main(){
 	/*Set the variables*/
-	char baseStr[NUM_INPUT][MAX_DIGIT];			//To save the base numbers array while user input
-	int power[NUM_INPUT];						//To save the power numbers of the base numbers
-	unsigned baseNum[NUM_INPUT][MAX_DIGIT];			//To save the integers of the base number
-	unsigned result[NUM_INPUT][MAX_RESULT_DIGIT];			//To save the results
-	unsigned resultTmp[NUM_INPUT][MAX_RESULT_DIGIT];		//To save the tmp when doing the calculation
-	int point[NUM_INPUT];						//To save the point of each numbers
-	int digitBase[NUM_INPUT];					//To save the digits of base numbers
-	int digitResult[NUM_INPUT];					//To save the digits of result
-	int i,j,k,l,m;							//To use for the loops
+	char baseStr[MAX_DIGIT];			//To save the base numbers array while user input
+	int power;						//To save the power numbers of the base numbers
+	unsigned baseNum[MAX_DIGIT];			//To save the integers of the base number
+	unsigned result[MAX_RESULT_DIGIT];			//To save the results
+	unsigned resultTmp[MAX_RESULT_DIGIT];		//To save the tmp when doing the calculation
+	int point;						//To save the point of each numbers
+	int digitBase;					//To save the digits of base numbers
+	int digitResult;					//To save the digits of result
+	int j,k,l,m;							//To use for the loops
 	int check;
-	int pointCheck[NUM_INPUT];
+	int pointCheck;
 	/*To get the inputs*/
-	for( i=0; i<NUM_INPUT; i++){
-		scanf("%s %d", baseStr[i], &power[i]);	//To get inputs
-		//printf("%s\n",baseStr[i]);
-		digitBase[i]=strlen(baseStr[i]);			//To save the total digits of each base numbers
-		memset(baseNum[i],0,sizeof(baseNum[i]));	//Set all initial digit to 0
-		memset(result[i],0,sizeof(result[i]));
-		memset(resultTmp[i],0,sizeof(resultTmp[i]));		
-	}
-	//printf("\npass inputs\n");
-	/*Transfer the base numbers from string to integer */
-	for( i=0; i<NUM_INPUT; i++){
+	while(scanf("%s%d",baseStr,&power)==2){
+		//printf("%s\n",baseStr);
+		digitBase=strlen(baseStr);			//To save the total digits of each base numbers
+		memset(baseNum,0,sizeof(baseNum));	//Set all initial digit to 0
+		memset(result,0,sizeof(result));
+		memset(resultTmp,0,sizeof(resultTmp));		
+		//printf("\npass inputs\n");
+		/*Transfer the base numbers from string to integer */
 		k=0;
-		point[i]=0;
-		pointCheck[i]=0;									
-		for( j=digitBase[i]-1; j>=0; j--){					//Loop untill hit the end of the string
-			//printf("%c\n",baseStr[i][j]);
-			if( (baseStr[i][j]<48) || (baseStr[i][j]>57)){//If the character is not a number, exit
-				if( (baseStr[i][j]=='.')){				//If the character is the point
-					point[i]=j;						//remeber the point position
-					digitBase[i]--;
+		point=0;
+		pointCheck=0;									
+		for( j=digitBase-1; j>=0; j--){					//Loop untill hit the end of the string
+			//printf("%c\n",baseStr[j]);
+			if( (baseStr[j]<48) || (baseStr[j]>57)){//If the character is not a number, exit
+				if( (baseStr[j]=='.')){				//If the character is the point
+					point=j;						//remeber the point position
+					digitBase--;
 					j--;
-					//printf("point[%d]=%d\n",i,point[i]);
-					pointCheck[i]=1;
+					//printf("point[%d]=%d\n",i,point);
+					pointCheck=1;
 				}
 				else{
 					exit(0);					//else will exit
 				}
 			}
-			baseNum[i][k]=baseStr[i][j]-'0';	//record the number
-			//printf("baseNum[%d][%d]=%d",i,k,baseNum[i][k]);
+			baseNum[k]=baseStr[j]-'0';	//record the number
+			//printf("baseNum[%d][%d]=%d",i,k,baseNum[k]);
 			k++;
 		}
-	}
-	//printf("\npass transfer\n");
-	/*Calculation*/
-	for( i=0; i<NUM_INPUT; i++){
-		resultTmp[i][0]=1;
-		digitResult[i]=1;											//set the tmp of result initial digit to 0
+		//printf("\npass transfer\n");
+		/*Calculation*/
+		resultTmp[0]=1;
+		digitResult=1;											//set the tmp of result initial digit to 0
 		//check=0;
-		for( j=0; j<power[i]; j++){
-			memset(result[i],0,sizeof(result[i]));					//Reset all of the numbers in the array of result[i] 
-			for( k=0; k<digitBase[i]; k++){
-				for( l=0; l<digitResult[i]; l++){					
-					result[i][k+l]+=resultTmp[i][l]*baseNum[i][k];	
-					if(result[i][k+l]>=10){						//Set less than 10 to avoid the number too large than the computer could store 
-						result[i][k+l+1]+=result[i][k+l]/10;
-						result[i][k+l]%=10;
+		for( j=0; j<power; j++){
+			memset(result,0,sizeof(result));					//Reset all of the numbers in the array of result 
+			for( k=0; k<digitBase; k++){
+				for( l=0; l<digitResult; l++){					
+					result[k+l]+=resultTmp[l]*baseNum[k];	
+					if(result[k+l]>=10){						//Set less than 10 to avoid the number too large than the computer could store 
+						result[k+l+1]+=result[k+l]/10;
+						result[k+l]%=10;
 					}
 				}
-				if(digitResult[i]<MAX_RESULT_DIGIT){					//To avoid the digitResult larger than MAX_RESULT_DIGIT
-					digitResult[i]=k+l;
+				if(digitResult<MAX_RESULT_DIGIT){					//To avoid the digitResult larger than MAX_RESULT_DIGIT
+					digitResult=k+l;
 				}
 			}
 			//printf("\n");
-			for(m=digitResult[i];m>=0;m--){
-				//printf("%d ",result[i][m]);
-				resultTmp[i][m]=result[i][m];
+			for(m=digitResult;m>=0;m--){
+				//printf("%d ",result[m]);
+				resultTmp[m]=result[m];
 			}
 			//printf("\n");
 		}
-		if(pointCheck[i]!=0){
-			point[i]=(digitBase[i]-point[i])*power[i];
+		if(pointCheck!=0){
+			point=(digitBase-point)*power;
 		}
-	}
-	//printf("\npass calculations\n");
-	for( i=0;i<NUM_INPUT;i++){									
+		//printf("\npass calculations\n");									
 		check=0;
 		m=0;
-		if((baseNum[i][0]==0)&&(digitBase[i]==1)){
+		if((baseNum[0]==0)&&(digitBase==1)){
 			printf("0");
 		}
-		else if(power[i]==0){
+		else if(power==0){
 			printf("1");
 		}
 		else{													
 			for( j=MAX_RESULT_DIGIT-1;j>=0;j--){
 				if(check==1){
-					if(result[i][j]==0){							//To remove the zero at the end of result numbers
+					if(result[j]==0){							//To remove the zero at the end of result numbers
 						m++;
 					}
 					else{
 						for(k=0;k<m;k++){
 							printf("0");
 						}
-						printf("%d",result[i][j]);
-						if((j==point[i]) && (point[i]!=0)){
+						printf("%d",result[j]);
+						if((j==point) && (point!=0)){
 							printf(".");
 						}
 						m=0;
 					}
 				}
-				else if(result[i][j]!=0){							//print the result start with a non-zero number
-					printf("%d",result[i][j]);
-					if((j==point[i]) && (point[i]!=0)){
+				else if(result[j]!=0){							//print the result start with a non-zero number
+					printf("%d",result[j]);
+					if((j==point) && (point!=0)){
 						printf(".");
 					}
 					check=1;
 				}
-				else if((j==point[i]) && (point[i]!=0)){							//This one is for the number less than one
+				else if((j==point) && (point!=0)){							//This one is for the number less than one
 					printf(".");
 					check=1;
 				}
