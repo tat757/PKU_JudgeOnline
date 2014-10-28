8e2525afe128fdba46012b113a7c8f15bc1a6f60
Yinsong Xu <yinsongxu@gmail.com>
2014-10-27 20:31:06 -0700

    Start 1002
    Start the problem 1002
    Add general information of the program
    Add main

---
+++ 1002/1002.c
@@ -0,0 +1,28 @@
+/*PKU Judge Online problem 1002 */
+/*			Yinsong Xu			*/
+/*Start Date:  Oct, 27	2014	*/
+/*End Date:    					*/
+/*Version:	   1.0				*/
+
+/*487-3279*/
+/*Currently, companies like to use some letters to replace numbers.*/
+/* This program is to check the letter+numbers combination and find out how many combination are represent same numbers.*/
+/*A,B,C = 2*/
+/*D,E,F = 3*/
+/*G,H,I = 4*/
+/*J,K,L = 5*/
+/*M,N,O = 6*/
+/*P,R,S = 7*/
+/*T,U,V = 8*/
+/*W,X,Y = 9*/
+
+/*Inputs: number of combinations, combinations of "-"+UppercaseLetters+numbers*/
+/*Outputs: numbers that 3digits+4digits separate by "-", and how many times it repeats*/
+
+#include <stdio.h>					//Base I/O headfile, to use scanf() printf()
+#include <string.h>					//To use memset()
+#define MAX_SIZE_INPUTS 20  		//Set the max size of inputs
+
+int main(){
+	return 0;
+}
